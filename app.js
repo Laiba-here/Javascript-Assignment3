d744a0a28291f9902beaff97d7dd25fbc5d79f2e
Laiba-here <nazlaiba230@gmail.com>
2024-09-28 06:22:51 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//         .... QUEATION 1-4 ....
+let age ="19";
+let track="14";
+let visitor="Laiba Naz";
+let product="pen";
+let Quantity="12";
+let birthYear="2003";
+alert("I am " + age + " years old");
+alert("welcome my web " + track + " times");
+document.write(" My birthday year is " + birthYear +"<br/>");
+document.write("Data type of my declared variable is number" + "<br/>" + "<br/>"+ "<br/>"+ "<br/>"+ "<br/>"+ "<br/>"+ "<br/>"+ "<br/>"+ "<br/>"+ "<br/>" )
+document.write( visitor  + " ordered " + Quantity  +  product + "(s) my webiste" );  
